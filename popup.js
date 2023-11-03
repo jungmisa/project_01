2d0fbce019684b1ea35de14a30797a0d56ee3c7f
jungmisa <decodubu07@gmail.com>
2023-11-04 01:23:37 +0900

    컨텐츠 패딩 수정

--- popup.js
+++ popup.js
@@ -3,3 +3,12 @@ $('.close').on('click', function(e){
 	$('.popup').hide();
 	$('.overlay').removeClass('open');		
 });
+
+//a버튼
+$('a').on('click', function (e) {
+    if ($(this).attr('href') == '#') {
+        e.preventDefault();
+    } else {
+
+    }
+})
